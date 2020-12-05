56713700c4830f2a53514cb3e83d0b06f47c8b59
Melissa <melissakheying@gmail.com>
2020-12-05 11:53:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This Project is all about Vanilla JS!
+
+Each topic is a separate JavaScript File:
+
+1. variables_and_dataTypes.js
+2.
+
+To view each JavaScript file, switch out the script in 'index.html' with the JS file of your choice!
+
---
+++ variables_and_dataTypes.js
@@ -0,0 +1,44 @@
+// Welcome to Variables and DataTypes!
+
+//TODO:
+// 1. Right click on index.html and click:
+//    'Reveal in Finder' - for mac
+// OR 'Reveal in File Explorer' - for Windows
+
+// 2. Double Click on index.html - which opens your local HTML file in the browser
+
+// 3. Right click in the browser and click 'inspect' to open up your 'dev tools'
+
+// 4. Click on 'Console' and see the console.log in action!!
+
+
+var variableOne = 'I am a var - Variable One';
+const variableTwo = 'I am a const - Variable Two';
+let variableThree = 'I am a let - Variable Three';
+
+console.log(variableOne);
+console.log(variableTwo);
+console.log(variableThree);
+
+// Variable Types are - var, const, & let
+
+// In depth detail on these types are provided in the article below
+
+// https://dev.to/sarah_chima/var-let-and-const--whats-the-difference-69e
+
+variableThree = 'My value has changed because I am a let variable'
+console.log(variableThree)
+
+
+// Values have a Type of Data stored in them
+
+// Data Types are:
+// Primitive: Strings, Numbers, Booleans, Null, Undefined
+// Complex: Object
+
+const stringType = 'I am a string because I am in quotes'
+const numberType = 3
+const booleanType = true
+const nullType = null
+const undefinedType = undefined
+const objectType = {'I am a string inside an object!'}
