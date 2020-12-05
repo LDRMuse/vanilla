3c9434067c82d1333596c2835f0a99d05419e4c0
Melissa <melissakheying@gmail.com>
2020-12-05 12:53:22 -0600

    :construction: create strings file

---
+++ strings.js
@@ -0,0 +1,33 @@
+// Welcome to Strings!
+
+//TODO: Remember to add this file to your 'index.js' script!!
+// 1. Right click on index.html and click:
+//    'Reveal in Finder' - for mac
+// OR 'Reveal in File Explorer' - for Windows
+
+// 2. Double Click on index.html - which opens your local HTML file in the browser
+
+// 3. Right click in the browser and click 'inspect' to open up your 'dev tools'
+
+// 4. Click on 'Console' and see the console.log in action!!
+
+
+
+// String - Data types that display text
+
+const emptyString = ''
+const singleQuotes = 'I am written in Single quotes'
+const doubleQuotes = "I am written in Double quotes"
+const backTicks = `I am written in Back Ticks`
+const coolThingAboutBackTicks = `I can have 'single' or "double" quotes written inside of me!!`
+
+const name = 'Melissa'
+const addMelissaToString = `Hello, my name is ${name}. My name was added to this string by using something called "Template Literals". `
+
+
+// Switch out the Variable that you'd like to see displayed in the console!
+//TODO: Don't forget to 'Refresh' your browser between switches!
+// Feel free to add as many console.log()'s as you'd like - I used one to keep the console 'cleaner'
+
+const changeMe = 'Change the variable in console.log() to see other variables in action!!'
+console.log(changeMe)
